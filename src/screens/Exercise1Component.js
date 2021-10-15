1edb1479eb3acd324c7c3ab7142208ecc6b4ee80
elbarea <rafaelbh15@hotmail.es>
2021-10-15 13:57:23 +0200

    Exercise 1 Solution

---
+++ src/screens/Exercise1Component.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const Exercise1Component = (props) => {
+  const name = "Rafael";
+
+  return (
+    <View>
+      <Text style={styles.font45}>Getting started with react native</Text>
+      <Text style={styles.font20}>My name is {name}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  font45: {
+    fontSize: 45,
+  },
+  font20: {
+    fontSize: 20,
+  },
+});
+ export default Exercise1Component;
