6f044c3015df1748c0c57221fbfa22d5b15da13d
elbarea <rafaelbh15@hotmail.es>
2021-10-20 13:33:46 +0200

    Props destructuring Extraer directamente los elementos que queremos de las props con la sintaxis { propiedad }

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,20 +1,18 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = (props) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>HomeScreen</Text>
       <Button
-        onPress={() => props.navigation.navigate("Components")}
+        onPress={() => navigation.navigate("Components")}
         title="Go to components demo"
       />
-      <TouchableOpacity onPress={() => props.navigation.navigate("List")}>
-        <Text>Go to list demo</Text>
-        <Text>Go to list demo</Text>
-        <Text>Go to list demo</Text>
-        <Text>Go to list demo</Text>
-      </TouchableOpacity>
+      <Button
+        onPress={() => navigation.navigate("List")}
+        title="Go to list demo"
+      />
     </View>
   );
 };
