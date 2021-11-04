1dddae840e97106399bb44e29e972bbc81eba23e
elbarea <rafaelbh15@hotmail.es>
2021-11-04 11:09:40 +0100

    Testing flexDirection property in BoxScreen

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -15,7 +15,9 @@ const styles = StyleSheet.create({
   viewStyle: {
     borderWidth: 3,
     borderColor: "black",
-    alignItems: "center"
+
+    flexDirection: "row",
+    height: 200
   },
   textStyle: {
     borderWidth: 3,
