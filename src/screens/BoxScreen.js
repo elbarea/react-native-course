e135afa2d1b1d71f2122f197a1b907c89fe5fb46
elbarea <rafaelbh15@hotmail.es>
2021-11-04 10:33:51 +0100

    Created BoxScreen file

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+
+const BoxScreen = () => {
+  return <Text>Box Screen</Text>;
+};
+
+const styles = StyleSheet.create({});
+export default BoxScreen;
