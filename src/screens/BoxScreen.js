5526bb5957629eedf141cd3c7d75cf6ab9a153bc
elbarea <rafaelbh15@hotmail.es>
2021-11-04 10:41:15 +0100

    Box Object Model tests in BoxScreen

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -1,9 +1,25 @@
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const BoxScreen = () => {
-  return <Text>Box Screen</Text>;
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>Box Screen</Text>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: "black",
+  },
+  textStyle: {
+    borderWidth: 10,
+    borderColor: "red",
+    margin: 20,
+    padding:10
+  },
+});
+
 export default BoxScreen;
